2dabffeb3b2de17f3e649331b445627f02b1f301
Pryesh Gohil <pryeshgohil@hotmail.com>
2024-02-01 14:59:10 +0000

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,7 +32,8 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
     }),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000 // 24 hours
 })
 
 
